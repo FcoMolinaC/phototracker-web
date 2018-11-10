21c09661e80fce7e6350db4292212fc69171dd88
FcoMolinaC <fmolinaprog@gmail.com>
2018-11-10 09:51:41 +0100

    Implementa el login con firebase.

---
+++ phototracker/login.js
@@ -0,0 +1,131 @@
+
+/**
+ * Handles the sign in button press.
+ */
+function toggleSignIn() {
+  if (firebase.auth().currentUser) {
+    firebase.auth().signOut();
+  } else {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    if (email.length < 4) {
+      alert('Please enter an email address.');
+      return;
+    }
+    if (password.length < 4) {
+      alert('Please enter a password.');
+      return;
+    }
+
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      if (errorCode === 'auth/wrong-password') {
+        alert('Wrong password.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+      document.getElementById('quickstart-sign-in').disabled = false;
+    });
+  }
+  document.getElementById('quickstart-sign-in').disabled = true;
+}
+
+/**
+ * Handles the sign up button press.
+ */
+function handleSignUp() {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  if (email.length < 4) {
+    alert('Please enter an email address.');
+    return;
+  }
+  if (password.length < 4) {
+    alert('Please enter a password.');
+    return;
+  }
+
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    if (errorCode == 'auth/weak-password') {
+      alert('The password is too weak.');
+    } else {
+      alert(errorMessage);
+    }
+    console.log(error);
+  });
+}
+
+/**
+ * Sends an email verification to the user.
+ */
+function sendEmailVerification() {
+  firebase.auth().currentUser.sendEmailVerification().then(function() {
+    alert('Email Verification Sent!');
+  });
+}
+
+function sendPasswordReset() {
+  var email = document.getElementById('email').value;
+
+  firebase.auth().sendPasswordResetEmail(email).then(function() {
+    alert('Password Reset Email Sent!');
+  }).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    if (errorCode == 'auth/invalid-email') {
+      alert(errorMessage);
+    } else if (errorCode == 'auth/user-not-found') {
+      alert(errorMessage);
+    }
+    console.log(error);
+  });
+}
+
+/**
+ * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+ *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+ *    out, and that is where we update the UI.
+ */
+function initApp() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    //document.getElementById('quickstart-verify-email').disabled = true;
+    if (user) {
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      //document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+      //document.getElementById('quickstart-sign-in').textContent = 'Cerrar sesión';
+      //document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+      //if (!emailVerified) {
+       //document.getElementById('quickstart-verify-email').disabled = false;
+      //}
+      window.open("index.html","_self")
+    } else {
+      //document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
+      document.getElementById('quickstart-sign-in').textContent = 'Iniciar sesión';
+      //document.getElementById('quickstart-account-details').textContent = 'null';
+    }
+    document.getElementById('quickstart-sign-in').disabled = false;
+  });
+
+  document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+  // document.getElementById('quickstart-sign-up').addEventListener('click', handleSignUp, false);
+  // document.getElementById('quickstart-verify-email').addEventListener('click', sendEmailVerification, false);
+  // document.getElementById('quickstart-password-reset').addEventListener('click', sendPasswordReset, false);
+}
+
+window.onload = function() {
+  initApp();
+};
