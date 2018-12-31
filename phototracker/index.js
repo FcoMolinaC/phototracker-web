5d14cad221c5c3b5c297c8dd033a95c8ad2a95de
FcoMolinaC <fmolinaprog@gmail.com>
2018-12-31 17:29:06 +0100

    Web: Modifica las tablas por entradas dinamicas. Android: Modifica el árbol de ruta para introducir datos.

--- phototracker/index.js
+++ phototracker/index.js
@@ -22,10 +22,54 @@ function currentUser() {
 	document.getElementById("current-user").innerHTML = email;
 }
 
+function userTracks() {
+	var myUserId = firebase.auth().currentUser.uid;
+	var ref = firebase.database().ref(myUserId + '/tracks');
+	var tracks = [];
+	var name, long, date, type;
+
+	ref.on("value", function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+			var key = childSnapshot.key;
+			var childData = childSnapshot.val();
+		  //console.log(childData["name"]);
+		  tracks.push(childData);
+
+		  //console.log(tracks);
+  	});
+
+		var result = "<table width=100%>";
+      result += "<thead>";
+      result += "<tr>";
+      result += "<th>Nombre</th>";
+      result += "<th>Distancia</th>";
+      result += "<th>Fecha</th>";
+      result += "<th>Tipo</th>";
+      result += "</tr>";
+      result += "</thead>";
+      result += "<tbody>";
+
+	    for(var i = 0; i < tracks.length; i++) {
+        result += "<tr>";
+        result += "<td>"+tracks[i]["name"]+"</td>";
+        result += "<td>"+tracks[i]["long"]+"</td>";
+        result += "<td>"+tracks[i]["date"]+"</td>";
+        result += "<td>"+tracks[i]["type"]+"</td>";
+        result += "</tr>";
+	    }
+
+      result += "</tbody>";
+	    result += "</table>";
+
+		document.getElementById("user-tracks").innerHTML = result;
+	});
+}
+
 function initApp() {
 	firebase.auth().onAuthStateChanged(function(user) {
 		if (user) {
 			currentUser();
+			userTracks();
 		} else {
 			// no user
 		}
