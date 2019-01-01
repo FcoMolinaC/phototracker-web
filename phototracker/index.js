1c27c9df678dfe32b8b6e4d747422fecec6f21c6
FcoMolinaC <fmolinaprog@gmail.com>
2019-01-01 13:17:03 +0100

    Web: Reinicia el array de rutas al actualizarse.

--- phototracker/index.js
+++ phototracker/index.js
@@ -1,83 +1,81 @@
-
 function signOut() {
-	firebase.auth().signOut().then(function() {
-		window.open("login.html","_self");
-	}).catch(function(error) {
-		return
-	});
+    firebase.auth().signOut().then(function() {
+        window.open("login.html", "_self");
+    }).catch(function(error) {
+        return
+    });
 }
 
 function currentUser() {
-	var user = firebase.auth().currentUser;
-	var name, email, photoUrl, uid, emailVerified;
+    var user = firebase.auth().currentUser;
+    var name, email, photoUrl, uid, emailVerified;
 
-	if (user != null) {
-	  name = user.displayName;
-	  email = user.email;
-	  photoUrl = user.photoURL;
-	  emailVerified = user.emailVerified;
-	  uid = user.uid;
-	}
+    if (user != null) {
+        name = user.displayName;
+        email = user.email;
+        photoUrl = user.photoURL;
+        emailVerified = user.emailVerified;
+        uid = user.uid;
+    }
 
-	document.getElementById("current-user").innerHTML = email;
+    document.getElementById("current-user").innerHTML = email;
 }
 
 function userTracks() {
-	var myUserId = firebase.auth().currentUser.uid;
-	var ref = firebase.database().ref(myUserId + '/tracks');
-	var tracks = [];
-	var name, long, date, type;
+    var myUserId = firebase.auth().currentUser.uid;
+    var ref = firebase.database().ref(myUserId + '/tracks');
+    var tracks = [];
+    var name, long, date, type;
 
-	ref.on("value", function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
-			var key = childSnapshot.key;
-			var childData = childSnapshot.val();
-		  //console.log(childData["name"]);
-		  tracks.push(childData);
+    ref.on("value", function(snapshot) {
+        tracks = [];
 
-		  //console.log(tracks);
-  	});
+        snapshot.forEach(function(childSnapshot) {
+            var key = childSnapshot.key;
+            var childData = childSnapshot.val();
+            //console.log(childData["name"]);
+            tracks.push(childData);
+            //console.log(tracks);
+        });
 
-		var result = "<table width=100%>";
-      result += "<thead>";
-      result += "<tr>";
-      result += "<th>Nombre</th>";
-      result += "<th>Distancia</th>";
-      result += "<th>Fecha</th>";
-      result += "<th>Tipo</th>";
-      result += "</tr>";
-      result += "</thead>";
-      result += "<tbody>";
-
-	    for(var i = 0; i < tracks.length; i++) {
+        var result = "<table width=100%>";
+        result += "<thead>";
         result += "<tr>";
-        result += "<td>"+tracks[i]["name"]+"</td>";
-        result += "<td>"+tracks[i]["long"]+"</td>";
-        result += "<td>"+tracks[i]["date"]+"</td>";
-        result += "<td>"+tracks[i]["type"]+"</td>";
+        result += "<th>Nombre</th>";
+        result += "<th>Distancia</th>";
+        result += "<th>Fecha</th>";
+        result += "<th>Tipo</th>";
         result += "</tr>";
-	    }
+        result += "</thead>";
+        result += "<tbody>";
 
-      result += "</tbody>";
-	    result += "</table>";
+        for (var i = 0; i < tracks.length; i++) {
+            result += "<tr>";
+            result += "<td>" + tracks[i]["name"] + "</td>";
+            result += "<td>" + tracks[i]["long"] + "</td>";
+            result += "<td>" + tracks[i]["date"] + "</td>";
+            result += "<td>" + tracks[i]["type"] + "</td>";
+            result += "</tr>";
+        }
 
-		document.getElementById("user-tracks").innerHTML = result;
-	});
+        result += "</tbody>";
+        result += "</table>";
+
+        document.getElementById("user-tracks").innerHTML = result;
+    });
 }
 
 function initApp() {
-	firebase.auth().onAuthStateChanged(function(user) {
-		if (user) {
-			currentUser();
-			userTracks();
-		} else {
-			// no user
-		}
-	});
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            currentUser();
+            userTracks();
+        } else {}
+    });
 
-	document.getElementById('sign-out').addEventListener('click', signOut, false);
+    document.getElementById('sign-out').addEventListener('click', signOut, false);
 }
 
 window.onload = function() {
-  initApp();
+    initApp();
 };
