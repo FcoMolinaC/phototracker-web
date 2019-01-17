b71191474fab5a3782fcdb4d15eaae3ea63b65ee
FcoMolinaC <fmolinaprog@gmail.com>
2019-01-17 21:47:38 +0100

    Implementa el renderizado de las rutas en el mapa. Enlaza las fotos georreferenciadas en el mapa.

--- phototracker/index.js
+++ phototracker/index.js
@@ -16,6 +16,8 @@ function currentUser() {
         photoUrl = user.photoURL;
         emailVerified = user.emailVerified;
         uid = user.uid;
+    } else {
+        console.error("No hay usuario logueado");
     }
 
     document.getElementById("current-user").innerHTML = email;
@@ -27,48 +29,98 @@ function userTracks() {
     var name, long, date, type;
 
     ref.on("value", function(snapshot) {
-        var tracks = [];
-
         snapshot.forEach(function(childSnapshot) {
             var childData = childSnapshot.val();
             tracks.push(childData);
         });
+        initMap();
 
-        var result = "<table width=100%>";
-        result += "<thead>";
-        result += "<tr>";
-        result += "<th>Nombre</th>";
-        result += "<th>Distancia</th>";
-        result += "<th>Fecha</th>";
-        result += "<th>Tipo</th>";
-        result += "</tr>";
-        result += "</thead>";
-        result += "<tbody>";
-
-        for (var i = 0; i < tracks.length; i++) {
+        if (typeof tracks !== 'undefined' && tracks.length > 0) {
+            var result = "<table width=100%>";
+            result += "<thead>";
             result += "<tr>";
-            result += "<td>" + tracks[i]["name"] + "</td>";
-            result += "<td>" + tracks[i]["long"] + "</td>";
-            result += "<td>" + tracks[i]["date"] + "</td>";
-            result += "<td>" + tracks[i]["type"] + "</td>";
+            result += "<th>Nombre</th>";
+            result += "<th>Distancia (metros)</th>";
+            result += "<th>Fecha</th>";
+            result += "<th>Tipo</th>";
             result += "</tr>";
+            result += "</thead>";
+            result += "<tbody>";
+
+            for (var i = 0; i < tracks.length; i++) {
+                result += "<tr onclick='trackLoad(rowIndex)'>";
+                result += "<td>" + tracks[i]["name"] + "</td>";
+                result += "<td>" + tracks[i]["long"] + "</td>";
+                result += "<td>" + tracks[i]["date"] + "</td>";
+                result += "<td>" + tracks[i]["type"] + "</td>";
+                result += "</tr>";
+            }
+
+            result += "</tbody>";
+            result += "</table>";
+
+            document.getElementById("user-tracks").innerHTML = result;
+            document.getElementById("user-tracks-count").innerHTML = tracks.length;
+
+        } else {
+            document.getElementById("user-tracks").innerHTML = "<p class='text-muted text-center my-5'>Puedes comenzar a grabar tracks desde la app Android</p>";
         }
-
-        result += "</tbody>";
-        result += "</table>";
-
-        document.getElementById("user-tracks").innerHTML = result;
-        document.getElementById("user-tracks-count").innerHTML = tracks.length;
     });
 }
+
+var tracks = [];
+var trackZoom = 12;
+var flightPath, map, photoMarker;
+
+function trackLoad(rowIndex){
+    var tracklong = parseFloat(tracks[rowIndex-1]["long"]);
+
+    if (tracklong >= 20000.00) {
+        trackZoom = 13;
+    } else if (tracklong >= 10000.00 && tracklong < 20000.00) {
+        trackZoom = 15;
+    } else if (tracklong >= 1000.00 && tracklong < 10000.00) {
+        trackZoom = 18;
+    } else {
+        trackZoom = 20;
+    }
+
+    map.setZoom(trackZoom);
+
+    var trackSelected = tracks[rowIndex-1]["track"];
+    if (typeof flightPath !== 'undefined') {
+        flightPath.setMap(null);
+        if (typeof photoMark !== 'undefined') {
+            photoMark.setMap(null);
+        }
+    }
+
+    flightPath = new google.maps.Polyline({
+      path: trackSelected.map(f => {
+                return {
+                    lat: f.latitude,
+                    lng: f.longitude,
+                }
+            }),
+      geodesic: true,
+      strokeColor: '#fc4c02',
+      strokeOpacity: 1.0,
+      strokeWeight: 2
+    });
+
+    map.setCenter({lat: trackSelected[0]['latitude'], lng: trackSelected[0]['longitude']});
+    flightPath.setMap(map);
+
+    event.stopPropagation();
+};
+
+var photos = [];
 
 function userPhotos() {
     var myUserId = firebase.auth().currentUser.uid;
     var ref = firebase.database().ref(myUserId + '/photos');
 
     ref.on("value", function(snapshot) {
-        var photos = [];
-
         snapshot.forEach(function(childSnapshot) {
             var childData = childSnapshot.val();
             photos.push(childData);
@@ -78,66 +130,58 @@ function userPhotos() {
 
         if (typeof photos !== 'undefined' && photos.length > 0) {
             for (var i = 0; i < photos.length; i++) {
-                result += "<a href=" + photos[i]["url"] + "><img src=" + photos[i]["url"] + "/></a>";
+                result += "<img onclick='photoLoad(" + i + ")' src=" + photos[i]["url"] + "class='img-fluid'>";
             }
 
             document.getElementById("user-photos").innerHTML = result;
             document.getElementById("user-photos-count").innerHTML = photos.length;
+        } else {
+            document.getElementById("user-photos").innerHTML = "<p class='text-muted text-center my-5'>Puedes comenzar a hacer fotos desde la app Android</p>";
         }
     });
 }
 
-function initMap() {
-    var myUserId = firebase.auth().currentUser.uid;
-    var ref = firebase.database().ref(myUserId + '/tracks');
-    var name, long, date, type;
-    var flightPlanCoordinates;
+var photoIndex = 1;
+var photoMark;
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'), {
-      zoom: 8,
-      center: {lat: 36.855686, lng: -2.418734},
+function photoLoad(i){
+    photoIndex = i;
+    flightPath.setMap(null);
+
+    var photoSelected = photos[i];
+    if (typeof photoMark !== 'undefined') {
+        photoMark.setMap(null);
+    }
+
+    map.setZoom(12);
+    map.setCenter({lat: photoSelected['latitude'], lng: photoSelected['longitude']});
+
+    photoMark = new google.maps.Marker({
+      position: {lat: photoSelected['latitude'], lng: photoSelected['longitude']},
+      map: map,
+      title: photoSelected['date']
+    });
+
+    event.stopPropagation();
+};
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map-canvas'), {
+      center: {lat: tracks[0]["track"][0]["latitude"], lng: tracks[0]["track"][0]["longitude"]},
       mapTypeId: 'terrain'
     });
-
-    ref.on("value", function(snapshot) {
-        var tracks = [];
-
-        snapshot.forEach(function(childSnapshot) {
-            var childData = childSnapshot.val();
-            tracks.push(childData);
-        });
-
-        for (var i = 0; i < tracks.length; i++) {
-            console.log(tracks[i]["track"]);
-            // flightPlanCoordinates = tracks[i]["track"];
-        }
-    });
-
-    // var flightPlanCoordinates = [
-    //   {lat: 36.855686, lng: -2.418734},
-    //   {lat: 36.564789, lng: -2.357899},
-    //   {lat: 36.478120, lng: -2.208888},
-    //   {lat: 36.322140, lng: -2.201445},
-    // ];
-    var flightPath = new google.maps.Polyline({
-      path: flightPlanCoordinates,
-      geodesic: true,
-      strokeColor: 'blue',
-      strokeOpacity: 1.0,
-      strokeWeight: 2
-    });
-
-    flightPath.setMap(map);
+    trackLoad(1);
 }
 
 function initApp() {
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
-            initMap();
             currentUser();
             userTracks();
             userPhotos();
-        } else {}
+        } else {
+            console.error("No hay usuario logueado");
+        }
     });
 
     document.getElementById('sign-out').addEventListener('click', signOut, false);
