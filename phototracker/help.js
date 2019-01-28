b6573bedc4c427275a5c1a6bd3ee74507cc3515a
FcoMolinaC <fmolinaprog@gmail.com>
2019-01-28 22:52:13 +0100

    Añade la página de ayuda.

---
+++ phototracker/help.js
@@ -0,0 +1,37 @@
+
+function signOut() {
+    firebase.auth().signOut().then(function() {
+        window.open("login.html", "_self");
+    }).catch(function(error) {
+        return
+    });
+}
+
+function currentUser() {
+    var user = firebase.auth().currentUser;
+    var name, email, photoUrl, uid, emailVerified;
+
+    if (user != null) {
+        name = user.displayName;
+        email = user.email;
+        photoUrl = user.photoURL;
+        emailVerified = user.emailVerified;
+        uid = user.uid;
+    }
+
+    document.getElementById("current-user").innerHTML = email;
+}
+
+function initApp() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            currentUser();
+        } else {}
+    });
+
+    document.getElementById('sign-out').addEventListener('click', signOut, false);
+}
+
+window.onload = function() {
+    initApp();
+};
